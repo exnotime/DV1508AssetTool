93acec70eb74b8fccd4b0be89570df7cd7b40794
Alexander <alleo92@hotmail.com>
2015-05-07 20:19:07 +0200

    works i think... camera orientation?

--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -27,14 +27,13 @@ void Game::Initialize(){
 
 	m_TestArea.SetPos(glm::vec2(640, 0));
 	m_TestArea.SetSize(glm::vec2(640, 720));
-
+	
 	///////////////////////////////////////////////////////////////////////////////
 	m_TestArea2.Initialize(glm::vec2(640, 720), glm::vec2(0, 0));
 	m_AutomaticRotate = false;
+	m_AutomaticRotateLeft = false;
 	m_MousePos = glm::vec2(0, 0);
 	m_PrevMousePos = glm::vec2(0, 0);
-//	m_StartRotationY = m_RotateY;
-	m_AutomaticRotateLeft = false;
 	m_Camera = gfx::g_GFXEngine.GetCamera();	
 	m_StartPos = m_Camera->GetPosition();
 	m_StartScale = m_Camera->GetPosition().z;
@@ -100,6 +99,7 @@ void Game::UpdateModelViewWindow(float p_deltaTime)
 		if (m_TestArea2.GetLeftMousePressed())
 		{//TODO: MOve camera instead
 			glm::vec3 newPos = m_Camera->GetPosition();
+
 			float x = 0, y = 0;
 			x = m_PrevMousePos.x - m_MousePos.x;
 			y = m_PrevMousePos.y - m_MousePos.y;
@@ -111,10 +111,34 @@ void Game::UpdateModelViewWindow(float p_deltaTime)
 			{
 				m_AutomaticRotateLeft = true;
 			}
-			newPos.x -= x * p_deltaTime;
-			newPos.y += y * p_deltaTime;
+			//X-led skall ändra x och z pos
+			//Y-led skall ändra y och z pos
+			if (x != 0.0f)
+			{
+				float angle = atan((x* p_deltaTime) / newPos.z);
+				float sinAngle = sin(angle);
+				float cosAngle = cos(angle);
+				float xPos = newPos.x;
+				float zPos = newPos.z;
+				newPos.x = xPos * cosAngle - zPos * sinAngle;
+				newPos.z = zPos * cosAngle + xPos * sinAngle;
 
-			m_Camera->SetPosition(newPos);
+				m_Camera->SetPosition(newPos);
+				//m_Camera->SetOrientation(glm::quat(0.0f, 0.0f, 0.0f, 1.0f));
+			}
+			if (y != 0.0f)
+			{
+				float angle = atan((y* p_deltaTime) / newPos.z);
+				float sinAngle = sin(angle);
+				float cosAngle = cos(angle);
+				float yPos = newPos.y;
+				float zPos = newPos.z;
+				newPos.y = yPos * cosAngle - zPos * sinAngle;
+				newPos.z = zPos * cosAngle + yPos * sinAngle;
+
+				m_Camera->SetPosition(newPos);
+				//m_Camera->SetOrientation(glm::quat(0.0f, 0.0f, 0.0f, 1.0f));
+			}
 		}
 		//Move the model around in the viewspace
 		if (m_TestArea2.GetRightMousePressed())
@@ -122,11 +146,10 @@ void Game::UpdateModelViewWindow(float p_deltaTime)
 			float x = 0, y = 0;
 			x = m_PrevMousePos.x - m_MousePos.x;
 			y = m_PrevMousePos.y - m_MousePos.y;
-			glm::vec3 temp = m_Camera->GetPosition();
-			temp.x += x * p_deltaTime;
-			temp.y -= y * p_deltaTime;
-			m_Camera->SetPosition(temp);
-
+			glm::vec3 newPos = m_Camera->GetPosition();
+			newPos.x += x * p_deltaTime;
+			newPos.y -= y * p_deltaTime;
+			m_Camera->SetPosition(newPos);
 		}
 		//Manipulate scale by scrolling the mouse wheel
 		m_Camera->MoveRelative(glm::vec3(0, 0, -m_TestArea2.GetMouseWheelState() * p_deltaTime * 20));
@@ -156,17 +179,28 @@ void Game::UpdateModelViewWindow(float p_deltaTime)
 		{
 			m_Camera->SetPosition(m_StartPos);
 		}
-		//Rotates automatically
-		if (m_AutomaticRotate)
-		{//TODO: MOve camera instead
-			if (!m_AutomaticRotateLeft)
-			{
-				m_RotateY += p_deltaTime;
-			}
-			else
-			{
-				m_RotateY -= p_deltaTime;
-			}
-		}
+	}
+	//Rotates automatically
+	if (m_AutomaticRotate)
+	{//TODO: MOve camera instead
+		glm::vec3 newPos = m_Camera->GetPosition();
+		float xDivZ;
+		if (m_AutomaticRotateLeft)
+			xDivZ = (p_deltaTime * 5) / newPos.z;
+		else
+			xDivZ = (-p_deltaTime * 5) / newPos.z;
+		float angle = atan(xDivZ);
+		float sinAngle = sin(angle);
+		float cosAngle = cos(angle);
+		float xPos = newPos.x;
+		float yPos = newPos.y;
+		float zPos = newPos.z;
+		newPos.x = xPos * cosAngle - zPos * sinAngle;
+		newPos.z = zPos * cosAngle + xPos * sinAngle;
+
+		//m_Camera->MoveRelative(newPos);
+		m_Camera->SetPosition(newPos);
+		m_Camera->SetOrientation(glm::quat(m_Pos.x, m_Pos.y, m_Pos.z, 0.0f));
+		//m_Camera->SetOrientation(glm::quat(1.0f, 0.0f, 0.0f, 0.0f));
 	}
 }
