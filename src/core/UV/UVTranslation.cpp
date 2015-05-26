bc4fcf3cbf7f84ef78d142d97ea3aee2f5f9ba35
Flow <johan_almeflo@hotmail.com>
2015-05-26 16:03:07 +0200

    Remove old code

--- src/core/UV/UVTranslation.cpp
+++ src/core/UV/UVTranslation.cpp
@@ -26,42 +26,7 @@ void UVTranslation::Initialize(int winWidth, int winHeight)
 	// 0 {1 1}
 	// 1 {0 0}
 	// 2 {0 1}
-
-	/*
-	// 1
-	m_uvPos1 = new float[2];
-	m_uvPos1[0] = vertices[(int)m_selectedFace.x].TexCoord.x;
-	m_uvPos1[1] = vertices[(int)m_selectedFace.x].TexCoord.y;
-
-	m_dotSprite1.SetTexture("asset/Cube/rDot.png");
-	m_dotSprite1.SetPos(glm::vec2(800.0f, 0.0f));
 	
-	// 2
-	m_uvPos2 = new float[2];
-	m_uvPos2[0] = vertices[(int)m_selectedFace.y].TexCoord.x;
-	m_uvPos2[1] = vertices[(int)m_selectedFace.y].TexCoord.y;
-
-	m_dotSprite2.SetTexture("asset/Cube/gDot.png");
-	m_dotSprite2.SetPos(glm::vec2(800.0f, 0.0f));
-
-	// 3
-	m_uvPos3 = new float[2];
-	m_uvPos3[0] = vertices[(int)m_selectedFace.z].TexCoord.x;
-	m_uvPos3[1] = vertices[(int)m_selectedFace.z].TexCoord.y;
-
-	m_dotSprite3.SetTexture("asset/Cube/bDot.png");
-	m_dotSprite3.SetPos(glm::vec2(800.0f, 0.0f));
-
-	// Temp (remove later)
-	// 4
-	m_uvPos4 = new float[2];
-	m_uvPos4[0] = vertices[3].TexCoord.x;
-	m_uvPos4[1] = vertices[3].TexCoord.y;
-
-	m_dotSprite4.SetTexture("asset/Cube/rDot.png");
-	m_dotSprite4.SetPos(glm::vec2(800.0f, 0.0f));
-	*/
-
 }
 
 void UVTranslation::Update(const float deltaTime)
@@ -73,43 +38,10 @@ void UVTranslation::Update(const float deltaTime)
 
 	ImGui::End();
 
-	/*
-	// 1
-	ImGui::SliderFloat2("UV Red", m_uvPos1, 0.0f, 1.0f);
-	m_dotSprite1.SetPos(glm::vec2(800.0f + m_uvPos1[0] * 800.0f - 7.5f, m_uvPos1[1] * 900.0f - 7.5f));
-
-	// 2
-	ImGui::SliderFloat2("UV Green", m_uvPos2, 0.0f, 1.0f);
-	m_dotSprite2.SetPos(glm::vec2(800.0f + m_uvPos2[0] * 800.0f - 7.5f, m_uvPos2[1] * 900.0f - 7.5f));
-
-	// 3
-	ImGui::SliderFloat2("UV Blue", m_uvPos3, 0.0f, 1.0f);
-	m_dotSprite3.SetPos(glm::vec2(800.0f + m_uvPos3[0] * 800.0f - 7.5f, m_uvPos3[1] * 900.0f - 7.5f));
-
-	// temp
-	// 4
-	ImGui::SliderFloat2("UV Red2", m_uvPos4, 0.0f, 1.0f);
-	m_dotSprite4.SetPos(glm::vec2(800.0f + m_uvPos4[0] * 800.0f - 7.5f, m_uvPos4[1] * 900.0f - 7.5f));
-	*/
 
 	if (m_uvModeOn)
 	{
 		std::vector<gfx::VertexPosNormalTexTangent>& vertices = gfx::g_ModelBank.GetVertices();
-
-		/*
-		vertices[(int)m_selectedFace.x].TexCoord.x = m_uvPos1[0];
-		vertices[(int)m_selectedFace.x].TexCoord.y = m_uvPos1[1];
-
-		vertices[(int)m_selectedFace.y].TexCoord.x = m_uvPos2[0];
-		vertices[(int)m_selectedFace.y].TexCoord.y = m_uvPos2[1];
-
-		vertices[(int)m_selectedFace.z].TexCoord.x = m_uvPos3[0];
-		vertices[(int)m_selectedFace.z].TexCoord.y = m_uvPos3[1];
-
-		// temp
-		vertices[3].TexCoord.x = m_uvPos4[0];
-		vertices[3].TexCoord.y = m_uvPos4[1];
-		*/
 
 		for (unsigned int i = 0; i < m_dots.size(); i++)
 		{
@@ -122,14 +54,8 @@ void UVTranslation::Draw(gfx::RenderQueue* renderQueue)
 {
 	if (m_uvModeOn)
 	{
-		/*
-		renderQueue->Enqueue(m_dotSprite1);
-		renderQueue->Enqueue(m_dotSprite2);
-		renderQueue->Enqueue(m_dotSprite3);
+		std::vector<gfx::VertexPosNormalTexTangent>& vertices = gfx::g_ModelBank.GetVertices();
 
-		// temp
-		renderQueue->Enqueue(m_dotSprite4);
-		*/
 		for (unsigned int i = 0; i < m_dots.size(); i++)
 		{
 			m_dots[i].Draw(renderQueue);
@@ -139,39 +65,7 @@ void UVTranslation::Draw(gfx::RenderQueue* renderQueue)
 
 void UVTranslation::Shutdown()
 {
-	/*
-	delete[] m_uvPos1;
-	delete[] m_uvPos2;
-	delete[] m_uvPos3;
-
-	delete[] m_uvPos4;
-	*/
 }
-
-//void UVTranslation::SelectFace(int vertex1, int vertex2, int vertex3)
-//{
-//	std::vector<gfx::VertexPosNormalTexTangent>& vertices = gfx::g_ModelBank.GetVertices();
-//
-//	if (vertex1 >= 0 && vertex1 < vertices.size()
-//		&& vertex2 >= 0 && vertex2 < vertices.size()
-//		&& vertex3 >= 0 && vertex3 < vertices.size())
-//	{
-//		m_selectedFace.x = (float)vertex1;
-//		m_selectedFace.y = (float)vertex2;
-//		m_selectedFace.z = (float)vertex3;
-//
-//		/*
-//		m_uvPos1[0] = vertices[(int)m_selectedFace.x].TexCoord.x;
-//		m_uvPos1[1] = vertices[(int)m_selectedFace.x].TexCoord.y;
-//
-//		m_uvPos2[0] = vertices[(int)m_selectedFace.y].TexCoord.x;
-//		m_uvPos2[1] = vertices[(int)m_selectedFace.y].TexCoord.y;
-//
-//		m_uvPos3[0] = vertices[(int)m_selectedFace.z].TexCoord.x;
-//		m_uvPos3[1] = vertices[(int)m_selectedFace.z].TexCoord.y;
-//		*/
-//	}
-//}
 
 void UVTranslation::AddVertex(int vertex)
 {
